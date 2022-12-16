cbd983851cd3c8657b2face75ac979a9c664c99f
User Name <kauth333@gmail.com>
2022-12-16 12:46:26 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# connect-four-unit-1-project
---
+++ js/app.js
@@ -0,0 +1,21 @@
+//1. Add HTML elements: div for container, <div> for board, div to show current player, restart button, auto-timer(if you win difficuty is increased as time span decreases each round ) , button option to play computer or another player.
+
+//2.) Use flexbox to create grid for board, disguish player 1 and player 2 pieces.
+//3.)Add cached element for : board, player, playAgain msg, restart button,player option, and  timer
+//4.)constant for winner, turn, time, box, tie
+//5.) constant for all possible winning combos 
+//5.)Add event listener for board, player,restart button, player option button  when clicked
+//6 a.) create init function (start timer in init funtion )
+//b.) create render funtion
+//c.) create updateBoard function
+//d.) create update msg function
+//7. Evoke updateboard  and updatemsg function in render function.
+//8. Create HandleClick funtion
+//9. Create PlacePiece function
+//10. Create checkforTie function
+//11. Create checkForWinner function
+//12. switchPlayerTurn Function
+//13. Reset functionality and Stop when timer runs out
+
+//AI OR NOT? 
+
